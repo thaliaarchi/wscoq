43c0419cc00c12e4cf4141c3ec16a46f28f8d1a2
Thalia Archibald <thalia@archibald.dev>
2022-12-15 13:22:47 -0700

    Make bin_to_string not tail-recursive to simplify proofs

--- Bin.v
+++ Bin.v
@@ -89,22 +89,33 @@ Definition bool_to_bit (b : bool) : bit :=
   | true => B1
   end.
 
-Definition bin_to_string (b : bin) : option string :=
-  let fix bin_to_string (b : bin) (s : string) : option string :=
-    match b with
-    | [] => Some s
-    | b0 :: b1 :: b2 :: b3 :: b4 :: b5 :: b6 :: b7 :: b' =>
-        bin_to_string b' (String (Ascii (bit_to_bool b0)
-                                        (bit_to_bool b1)
-                                        (bit_to_bool b2)
-                                        (bit_to_bool b3)
-                                        (bit_to_bool b4)
-                                        (bit_to_bool b5)
-                                        (bit_to_bool b6)
-                                        (bit_to_bool b7)) s)
-    | _ => None
-    end in
-  bin_to_string b EmptyString.
+Theorem bit_bool_bit : forall b,
+  bool_to_bit (bit_to_bool b) = b.
+Proof. intros []; reflexivity. Qed.
+
+Theorem bool_bit_bool : forall b,
+  bit_to_bool (bool_to_bit b) = b.
+Proof. intros []; reflexivity. Qed.
+
+Fixpoint bin_to_string (b : bin) : option string :=
+  match b with
+  | [] => Some EmptyString
+  | b0 :: b1 :: b2 :: b3 :: b4 :: b5 :: b6 :: b7 :: b' =>
+      match bin_to_string b' with
+      | None => None
+      | Some s =>
+          Some (String (Ascii (bit_to_bool b0)
+                              (bit_to_bool b1)
+                              (bit_to_bool b2)
+                              (bit_to_bool b3)
+                              (bit_to_bool b4)
+                              (bit_to_bool b5)
+                              (bit_to_bool b6)
+                              (bit_to_bool b7))
+                       s)
+      end
+  | _ => None
+  end.
 
 Fixpoint string_to_bin (s : string) : bin :=
   match s with
@@ -120,6 +131,24 @@ Fixpoint string_to_bin (s : string) : bin :=
           bool_to_bit b7 ::
           string_to_bin s'
   end.
+
+Theorem bin_string_bin : forall b s,
+  to_string b = Some s -> of_string s = b.
+Proof.
+  induction b as [|b0 [|b1 [|b2 [|b3 [|b4 [|b5 [|b6 [|b7 b']]]]]]]];
+  intros; inversion H; subst; clear H.
+  - reflexivity.
+  - generalize dependent s.
+    induction b'. cbn in *. Admitted.
+
+Theorem string_bin_string : forall s,
+  bin_to_string (string_to_bin s) = Some s.
+Proof.
+  induction s.
+  - reflexivity.
+  - destruct a. cbn. repeat rewrite bool_bit_bool.
+    rewrite IHs. reflexivity.
+Qed.
 
 Fixpoint positive_to_nat (p : positive) : nat :=
   match p with
