052d5eab598cdaf684c479e079bb9aea2eaed1ac
Thalia Archibald <thalia@archibald.dev>
2023-02-01 16:21:16 -0700

    Move relation variables from forall to constructors

--- Lang.v
+++ Lang.v
@@ -54,103 +54,103 @@ Definition retrieve (hp : list Z) (n : nat) : Z :=
   nth_default 0 hp n.
 
 Inductive step : vm -> vm -> Prop :=
-  | SPush : forall prog stk hp sin sout pc calls n,
+  | SPush prog stk hp sin sout pc calls n :
       nth_error prog pc = Some (IPush n) ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog (n :: stk) hp sin sout (S pc) calls)
-  | SDup : forall prog x stk' hp sin sout pc calls,
+  | SDup prog x stk' hp sin sout pc calls :
       nth_error prog pc = Some IDup ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog (x :: x :: stk') hp sin sout (S pc) calls)
-  | SCopy : forall prog stk hp sin sout pc calls n x,
+  | SCopy prog stk hp sin sout pc calls n x :
       nth_error prog pc = Some (ICopy n) ->
       nth_error stk n = Some x ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog (x :: stk) hp sin sout (S pc) calls)
-  | SSwap : forall prog x y stk' hp sin sout pc calls,
+  | SSwap prog x y stk' hp sin sout pc calls :
       nth_error prog pc = Some (ISwap) ->
       step (VM prog (x :: y :: stk') hp sin sout pc calls)
            (VM prog (y :: x :: stk') hp sin sout (S pc) calls)
-  | SDrop : forall prog x stk' hp sin sout pc calls,
+  | SDrop prog x stk' hp sin sout pc calls :
       nth_error prog pc = Some (IDrop) ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog stk' hp sin sout (S pc) calls)
-  | SSlide : forall prog x stk' hp sin sout pc calls n,
+  | SSlide prog x stk' hp sin sout pc calls n :
       nth_error prog pc = Some (ISlide n) ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog (x :: skipn n stk') hp sin sout (S pc) calls)
-  | SAdd : forall prog y x stk' hp sin sout pc calls,
+  | SAdd prog y x stk' hp sin sout pc calls :
       nth_error prog pc = Some IAdd ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog (x + y :: stk') hp sin sout (S pc) calls)
-  | SSub : forall prog y x stk' hp sin sout pc calls,
+  | SSub prog y x stk' hp sin sout pc calls :
       nth_error prog pc = Some ISub ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog (x - y :: stk') hp sin sout (S pc) calls)
-  | SMul : forall prog y x stk' hp sin sout pc calls,
+  | SMul prog y x stk' hp sin sout pc calls :
       nth_error prog pc = Some IMul ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog (x * y :: stk') hp sin sout (S pc) calls)
-  | SDiv : forall prog y x stk' hp sin sout pc calls,
+  | SDiv prog y x stk' hp sin sout pc calls :
       nth_error prog pc = Some IDiv ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog (x / y :: stk') hp sin sout (S pc) calls)
-  | SMod : forall prog y x stk' hp sin sout pc calls,
+  | SMod prog y x stk' hp sin sout pc calls :
       nth_error prog pc = Some IMod ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog (x mod y :: stk') hp sin sout (S pc) calls)
-  | SStore : forall prog y x n stk' hp sin sout pc calls,
+  | SStore prog y x n stk' hp sin sout pc calls :
       nth_error prog pc = Some IStore ->
       Z_to_nat x = Some n ->
       step (VM prog (y :: x :: stk') hp sin sout pc calls)
            (VM prog stk' (store hp n y) sin sout (S pc) calls)
-  | SRetrieve : forall prog x n stk' hp sin sout pc calls,
+  | SRetrieve prog x n stk' hp sin sout pc calls :
       nth_error prog pc = Some IRetrieve ->
       Z_to_nat x = Some n ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog (retrieve hp n :: stk') hp sin sout (S pc) calls)
-  | SLabel : forall prog stk hp sin sout pc calls l,
+  | SLabel prog stk hp sin sout pc calls l :
       nth_error prog pc = Some (ILabel l) ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog stk hp sin sout (S pc) calls)
-  | SCall : forall prog stk hp sin sout pc calls l,
+  | SCall prog stk hp sin sout pc calls l :
       nth_error prog pc = Some (ICall l) ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog stk hp sin sout l (S pc :: calls))
-  | SJmp : forall prog stk hp sin sout pc calls l,
+  | SJmp prog stk hp sin sout pc calls l :
       nth_error prog pc = Some (IJmp l) ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog stk hp sin sout l calls)
-  | SJz : forall prog x stk' hp sin sout pc calls l,
+  | SJz prog x stk' hp sin sout pc calls l :
       nth_error prog pc = Some (IJz l) ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog stk' hp sin sout (if x =? 0 then l else S pc) calls)
-  | SJn : forall prog x stk' hp sin sout pc calls l,
+  | SJn prog x stk' hp sin sout pc calls l :
       nth_error prog pc = Some (IJn l) ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog stk' hp sin sout (if x <? 0 then l else S pc) calls)
-  | SRet : forall prog stk hp sin sout pc l calls,
+  | SRet prog stk hp sin sout pc l calls :
       nth_error prog pc = Some IRet ->
       step (VM prog stk hp sin sout pc (l :: calls))
            (VM prog stk hp sin sout l calls)
-  | SEnd : forall prog stk hp sin sout pc calls,
+  | SEnd prog stk hp sin sout pc calls :
       nth_error prog pc = Some IEnd ->
       step (VM prog stk hp sin sout pc calls)
            (VM prog stk hp sin sout (length prog) calls)
-  | SPrintc : forall prog x stk' hp sin sout pc calls,
+  | SPrintc prog x stk' hp sin sout pc calls :
       nth_error prog pc = Some IPrintc ->
       step (VM prog (x :: stk') hp sin sout pc calls)
            (VM prog stk' hp sin (IOChar x :: sout) (S pc) calls)
-  | SPrinti : forall prog v stk' hp sin sout pc calls,
+  | SPrinti prog v stk' hp sin sout pc calls :
       nth_error prog pc = Some IPrinti ->
       step (VM prog (v :: stk') hp sin sout pc calls)
            (VM prog stk' hp sin (IOInt v :: sout) (S pc) calls)
-  | SReadc : forall prog x n stk' hp v sin sout pc calls,
+  | SReadc prog x n stk' hp v sin sout pc calls :
       nth_error prog pc = Some IReadc ->
       Z_to_nat x = Some n ->
       step (VM prog (x :: stk') hp (IOChar v :: sin) sout pc calls)
            (VM prog stk' (store hp n v) sin sout (S pc) calls)
-  | SReadi : forall prog x n stk' hp v sin sout pc calls,
+  | SReadi prog x n stk' hp v sin sout pc calls :
       nth_error prog pc = Some IReadi ->
       Z_to_nat x = Some n ->
       step (VM prog (x :: stk') hp (IOInt v :: sin) sout pc calls)
@@ -158,9 +158,9 @@ Inductive step : vm -> vm -> Prop :=
 
 (* Equivalent to clos_refl_trans_1n in Coq.Relations.Relation_Operators. *)
 Inductive execute : vm -> vm -> Prop :=
-  | execute_refl : forall (x : vm),
+  | execute_refl x :
       execute x x
-  | execute_step : forall (x y z : vm),
+  | execute_step x y z :
       step x y ->
       execute y z ->
       execute x z.
